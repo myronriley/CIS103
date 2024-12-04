65833b626f76a001cac4f2e36b26d3ea5959cedb
myronriley <mriley33@student.ccc.edu>
2024-12-03 19:38:42 -0600

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,16 @@
+width = float(input('enter width of rectangle: '))
+height = float(input('enter height of rectangle: '))
+
+area = width * height
+print('area is ',area)
+
+width = float(input('enter base of triangle: '))
+height = float(input('enter height of triangle: '))
+
+area = width * height * .5
+print('area is ',area)
+
+radius = float(input('enter base of circle: '))
+area = radius **2 * 3.14159
+
+print('area is ',area)
